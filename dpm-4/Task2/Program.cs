5364e6c8288569bce07f51b5832ca22dc88d35a5
NKVDream <Kmv235@yandex.ru>
2025-03-29 07:17:53 -0700

    2 done

---
+++ dpm-4/Task2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+class Task2
+{
+    static void Main()
+    {
+        int[] RanNum = new int[10];
+        Random random = new Random();
+        for (int i = 0; i < RanNum.Length; i++)
+        {
+            RanNum[i] = random.Next(-100, 100);
+        }
+        foreach(int i in RanNum)
+        {
+            Console.Write($"{i}\t");
+        }
+        Task2.Reverse(RanNum);
+    }
+    static void Reverse(int[]array)
+    {
+
+        int n = array.Length; // длина массива
+        int k = n / 2;          // середина массива
+        int temp;               // вспомогательный элемент для обмена значениями
+        for (int i = 0; i < k; i++)
+        {
+            temp = array[i];
+            array[i] = array[n - i - 1];
+            array[n - i - 1] = temp;
+        }
+        foreach (int i in array)
+        {
+            Console.Write($"{i} \t");
+        }
+    }
+}
